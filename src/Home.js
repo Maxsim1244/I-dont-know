90a062f075918257c02012ee724d271ba727144d
Maxsim1244 <joper1234@yandex.com>
2022-01-18 02:53:22 +0500

    ccccc

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -16,6 +16,9 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
 
   return (
     <div className="home">
