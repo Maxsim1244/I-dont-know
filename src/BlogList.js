ba54f87dfc42c2a873cd32f42f849df03ce837bf
Maxsim1244 <joper1234@yandex.com>
2022-02-01 18:30:41 +0500

    search feature

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,36 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 const BlogList = ({ blogs, title, handleDelete }) => {
+  const [search, setSearch] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState(blogs);
+  const handleInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const searchMod = search.toLowerCase().trim();
+    const newBlogs = blogs.filter(
+      (a) =>
+        a.title.toLowerCase().trim().includes(searchMod) ||
+        a.body.toLowerCase().trim().includes(searchMod) ||
+        a.author.toLowerCase().trim().includes(searchMod)
+    );
+    setFilteredBlogs(newBlogs);
+  }, [search]);
+
   return (
     <div className="blog-list">
+      <input
+        className="search-input"
+        type="text"
+        value={search}
+        onInput={handleInput}
+        placeholder="Search"
+      />
       <h2>{title}</h2>
-      {blogs.map((blog) => (
+      {filteredBlogs.map((blog) => (
         <div key={blog.id} className="blog-preview">
           <Link to={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
